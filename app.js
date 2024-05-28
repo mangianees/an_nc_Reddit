d9693fecceda03f3b92064feaf8fca9def0c0526
mangianees <mangianees@gmail.com>
2024-05-28 14:18:49 +0100

    Task 3 core Get Api completed

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express')
 
 const {getTopics} = require('./Controllers/topics.controllers')
 
+const {getApi} = require('./Controllers/api.controllers')
+
 const app = express();
 
 app.use(express.json())
 
 app.get('/api/topics',getTopics)
+
+app.get('/api',getApi)
 
 module.exports = app;
 
