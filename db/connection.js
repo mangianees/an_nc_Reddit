f891fb30ccad7ff2baafb4536b529ba775d5cc71
mangianees <mangianees@gmail.com>
2024-05-31 13:34:47 +0100

    Hosting in progress

--- db/connection.js
+++ db/connection.js
@@ -3,12 +3,17 @@ const ENV = process.env.NODE_ENV || 'development';
 const pathToEnvFile=`${__dirname}/../.env.${ENV}`;
 
 require('dotenv').config({
-  // path: `${__dirname}/../.env.${ENV}`,
-
   path:pathToEnvFile
 });
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
+const config = {};
+
+if (ENV === 'production') {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
 module.exports = new Pool();
