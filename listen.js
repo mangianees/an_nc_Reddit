9d56d17b12cac5b00b4936cd80769b73e84ecd5b
mangianees <mangianees@gmail.com>
2024-06-17 11:02:08 +0100

    listen.js updated

--- listen.js
+++ listen.js
@@ -1,5 +1,5 @@
-const app = require('./app')
 
-app.listen(9090,()=>{
-    console.log('App is listening on 9090');
-})
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
